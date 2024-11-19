ba48f1ad0582551660396f8f74dd94cf84d29638
SeHoon <wer5136@naver.com>
2024-11-19 10:47:45 +0900

    image

--- price_increase/webapp/controller/Price_Increase_View.controller.js
+++ price_increase/webapp/controller/Price_Increase_View.controller.js
@@ -12,26 +12,26 @@ function (Controller, Filter, FilterOperator, MessageToast, Device, JSONModel) {
     return Controller.extend("zc501sd.cds0001.priceincrease.controller.Price_Increase_View", {
         onInit: function () 
         {
-            let oTable = this.getView().byId("priceList"),  // 현재 View에서 사용할 Table
-                oBinding = oTable.getBinding("rows"),       // Binding으로 EntitySet 가져옴
-                oFilter  = null;                            // 검색조건
-                // aFilter  = []; (나는 검색조건이 1개라 이거 안해도됨)  
+            // let oTable = this.getView().byId("priceList"),  // 현재 View에서 사용할 Table
+            //     oBinding = oTable.getBinding("rows"),       // Binding으로 EntitySet 가져옴
+            //     oFilter  = null;                            // 검색조건
+            //     // aFilter  = []; (나는 검색조건이 1개라 이거 안해도됨)  
                 
-            oFilter = new Filter("Mtart", FilterOperator.EQ, "FERT" );
+            // oFilter = new Filter("Mtart", FilterOperator.EQ, "FERT" );
             
-            // oBinding.filter(oFilter);
+            // // oBinding.filter(oFilter);
 
-            // Image Test
-            // Image Test
+            // // Image Test
+            // // Image Test
 
-            // var bIsPhone = Device.system.phone,
-			// svgLogo = sap.ui.require.toUrl("img"),
-			// oImgModel;
+            // // var bIsPhone = Device.system.phone,
+			// // svgLogo = sap.ui.require.toUrl("img"),
+			// // oImgModel;
 
-			// this.getView().setModel(new JSONModel({
-			// 	imageWidth: bIsPhone ? "5em" : "10em",
-			// 	svgLogo: svgLogo
-			// }));
+			// // this.getView().setModel(new JSONModel({
+			// // 	imageWidth: bIsPhone ? "5em" : "10em",
+			// // 	svgLogo: svgLogo
+			// // }));
 
 			// // set explored app's demo model on this sample
 			// oImgModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/img.json"));
